73852e9cb707972c2dd65ceb3b5c2c19ecce3d41
Joann Brady <jbrady11934@optimum.net>
2020-04-16 15:05:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number)
   number -= 1
 end
 return "HAPPY NEW YEAR!"
-
+end
 
 
 def countdown_with_sleep(number)
@@ -15,5 +15,6 @@ def countdown_with_sleep(number)
   sleep(1)
   number -= 1
 end
-end
-puts "HAPPY NEW YEAR!"
+puts
+
+"HAPPY NEW YEAR!"
